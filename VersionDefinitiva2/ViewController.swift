27acc9a5420b9a0493efd88cefdb3d0e52d66ada
Conrado Delso Gonzalez <34092844+Conrado97@users.noreply.github.com>
2018-06-15 07:36:20 +0200

    Update ViewController.swift

--- VersionDefinitiva2/ViewController.swift
+++ VersionDefinitiva2/ViewController.swift
@@ -9,113 +9,89 @@
 import UIKit
 import Firebase
 import SwiftKeychainWrapper
-import FirebaseStorage
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var userImgView: UIImageView!
+    
     @IBOutlet weak var usernameField: UITextField!
+    
     @IBOutlet weak var emailField: UITextField!
     
     @IBOutlet weak var passwordField: UITextField!
     
-    let ref = Database.database().reference()
-    var imagePicker: UIImagePickerController
+    var imagePicker: UIImagePickerController!
     var selectedImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         imagePicker = UIImagePickerController()
         imagePicker.allowsEditing = true
         imagePicker.delegate = self
-        
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         if let _ = KeychainWrapper.standard.string(forKey: "uid") {
             self.performSegue(withIdentifier: "toFeed", sender: nil)
         }
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func storeUserData(userId : String) {
-        let downloadURL: String!
-        
-        if let imageData = UIImageJPEGRepresentation(selectedImage, 0.2) {
-            
-          let imgUid = NSUUID().uuidString
-            
-        let metaData = StorageMetadata()
-            
-        let uploadTask = storage.reference().putData(imageData, metadata: nil) { (metadata, error)
-            in
-            guard let metadata = metadata else {
-                return
-            }
-            downloadURL = metadata.downloadURL
-            self.ref.child("users").child(userId).setValue(
-                "username": usernameField.text,
-                "userImg": downloadURL
-                ])
-            
+    func setupUser(userUid: String) {
+        if let imageData = UIImageJPEGRepresentation(self.userImgView.image!, 0.2) {
+            let imgUid = NSUUID().uuidString
+            let metaData = StorageMetadata()
+            Storage.storage().reference().child(imgUid).putData(imageData, metadata: metaData) { (metadata, error) in
+                
+                let downloadURL = metadata?.downloadURL()?.absoluteString
+                
+                let userData = [
+                    "username": self.usernameField.text!,
+                    "userImg": downloadURL!
+                    ] as [String : Any]
+                
+                Database.database().reference().child("users").child(userUid).setValue(userData)
+                self.performSegue(withIdentifier: "toFeed", sender: nil)
             }
         }
-      
     }
 
-    
-        func signInpressed(_ sender: Any) {
+    @IBAction func signInPressed(_ sender: Any) {
         if let email = emailField.text, let password = passwordField.text {
-            Auth.auth().signIn(withEmail: email, password: password)
-            { (user, error)
-                in
-                if error != nil && !(self.usernameField.text?.isEmpty)! &&
-                    self.userImgView.image != nil {
-                    Auth.auth().createUser(withEmail: email, password:password)
-                    { (user, error) in
-                        self.storeUserData(userId: (user?.uid)!)
-                        KeychainWrapper.standard.set(user?.uid)!, forKey:"uid")
-                        self.performSegue(withIdentifier: "toFeed", sender: nil)
+            Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                if error != nil && !(self.usernameField.text?.isEmpty)! {
+                    Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                        self.setupUser(userUid: (user?.uid)!)
+                        KeychainWrapper.standard.set((user?.uid)!, forKey: "uid")
                     }
-                    
-                   
                 } else {
                     if let userID = user?.uid {
-                        KeychainWrapper.standard.set((userID), forKey:"uid")
-                    self.performSegue(withIdentifier: "toFeed", sender: nil)
+                        KeychainWrapper.standard.set((userID), forKey: "uid")
+                        self.performSegue(withIdentifier: "toFeed", sender: nil)
+                    }
                 }
-            }    }
-    }
-
-}
-}
-}
-
-extension ViewController: UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate{
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        didFinishPickingMediaWithInfo info: [String : Any]) do {
-            if let image = info[UIImagePickerController] as? UIImage {
-                selectedImage = image
             }
-            imagePicker.didmiss(animated: true, completion: nil)
         }
+    }
     
-    
-    
+    @IBAction func getPhoto (_ sender: AnyObject) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            userImgView.image = image
+        } else {
+            print("image wasnt selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 }
-
-
-
-
-
-
-
-
